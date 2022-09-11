50a6446e676b0f8c278639e4a43da829f2fd71cc
Ajay Pathak <apathak2@slb.com>
2022-09-11 10:13:06 +0530

    First Program

---
+++ triangle.py
@@ -0,0 +1,24 @@
+def triangle(n,symbol):
+     
+    # number of spaces
+    k = n - 1
+ 
+    
+    for i in range(0, n):
+     
+        
+        for j in range(0, k):
+            print(end=" ")
+     
+        
+        k = k - 1
+     
+        for j in range(0, i+1):
+            print(symbol+" ", end="")
+             
+        print("\r")
+ 
+# Driver Code
+n = 5
+symbol="#"
+triangle(n,symbol)
